0060c7667e76491df0af2bef029a932421d544e3
Rafał Lewandowski <rafal.lewandowski@swingdev.io>
2019-04-10 15:48:39 +0200

    Implement additional HealthCheck class tests

--- test/base/HealthCheck.spec.ts
+++ test/base/HealthCheck.spec.ts
@@ -5,13 +5,35 @@ describe('HealthCheck', () => {
   describe('check', () => {
     it('should forward doCheck return value', async () => {
       const healthStatus: HealthStatus = HealthStatus.UP('I am healthy!')
-      class DummyHealthCheck extends HealthCheck {
+      class HealthyHealthCheck extends HealthCheck {
         protected async doCheck (): Promise<HealthStatus> {
           return healthStatus
         }
       }
 
-      expect(await new DummyHealthCheck().check()).toEqual(healthStatus)
+      await expect(new HealthyHealthCheck().check()).resolves.toEqual(healthStatus)
+    })
+
+    it('should reject with error thrown by doCheck', async () => {
+      const error: Error = new Error('Something bad happened!!!')
+      class ThrowingHealthStatus extends HealthCheck {
+        protected async doCheck(): Promise<HealthStatus> {
+          throw error
+        }
+      }
+
+      await expect(new ThrowingHealthStatus().check()).rejects.toEqual(error)
+    })
+
+    it('should reject with rejection done by doCheck', async () => {
+      const rejectionReason: string = 'Uh oh'
+      class RejectingHealthStatus extends HealthCheck {
+        protected async doCheck(): Promise<HealthStatus> {
+          return Promise.reject(rejectionReason)
+        }
+      }
+
+      await expect(new RejectingHealthStatus().check()).rejects.toEqual(rejectionReason)
     })
   })
 })
